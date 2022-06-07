736a620fb25990fe1bb82b5b2e6b5dd259e6673b
Morgan Allen <morganallen@morgans-air.lan>
2022-06-06 18:32:17 -0600

    partial

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAdd();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+    });
+  }
+  
   render() {
+    console.log(this.state.input)
     return (
-      <div>
-        Form
-      </div>
+      <form>
+          <input onChange={this.handleChange}/>
+          <button onClick={this.handleSubmit}>Add</button>
+        </form>
     )
   }
 }
